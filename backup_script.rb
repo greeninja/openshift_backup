4c5dbf7e15f750496ac96b06c6db4d7ed2a25b53
James Eckersall <james.eckersall@fasthosts.com>
2016-07-19 12:09:00 +0100

    more fixes

--- backup_script.rb
+++ backup_script.rb
@@ -2,7 +2,7 @@
 require 'fileutils'
 require 'yaml'
 require 'time'
-require 'pp'
+require 'logger'
 
 # metadata required for backups on pods
 # metadata.labels.needs_backup: yes
@@ -14,7 +14,6 @@ require 'pp'
 
 # backup destination tree
 # project/dc/pod_name/container_name/yyyy-mm/
-# each backup dir is a git repo
 
 if ENV.include? 'DEBUG' then
   DEBUG=ENV['DEBUG']
@@ -22,33 +21,46 @@ else
   DEBUG=false
 end
 
+log = Logger.new("| tee backup_script.log")
+if DEBUG then
+  log.level = Logger::DEBUG
+else
+  log.level = Logger::INFO
+end
+
+if ENV.include? 'FAKESYSTEM' then
+  FAKESYSTEM=ENV['FAKESYSTEM']
+else
+  FAKESYSTEM=false
+end
+
 BACKUP_DEST_ROOT='backup-data'
 
 def die (msg)
-  puts msg
+  log.info msg
   exit 1
 end
 
 if File.exists? '/var/run/secrets/kubernetes.io/serviceaccount/token' then
-  puts "Logging into openshift with serviceaccount" if DEBUG
+  log.info "Logging into openshift with serviceaccount"
   ret = system('oc login https://openshift-cluster.fhpaas.fasthosts.co.uk:8443 --token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)')
 end
 ret = system('oc status >/dev/null 2>&1')
 die "Unable to log into openshift" unless ret
 
-# make sure we have git config correct
-`git config --global user.name "Backup Service"` unless system('git config --global user.name >/dev/null 2>&1')
-`git config --global user.email "linuxteam@fasthosts.com"` unless system('git config --global user.email > /dev/null 2>&1')
-
 class PodBackup
-  def initialize (pod_spec={}, backup_dest_root='.')
+  def initialize (pod_spec={}, backup_dest_root='.', log)
+    @log = log
     # metadata should be the output from "oc get pod x -o yaml"
+    @backup_time = Time.now.strftime("%Y-%m-%d_%H:%M")
     @pod_spec = pod_spec
     @metadata = @pod_spec['metadata']
     @podname = @metadata['name']
     @project = @metadata['namespace']
+    @dc = @metadata['labels']['deploymentconfig']
     @backup_dest_root = backup_dest_root
-    @backup_dest = "#{backup_dest_root}/#{@project}/#{@podname}"
+    @backup_dest_pod_root = "#{backup_dest_root}/#{@project}/#{@dc}/#{@backup_time}/#{@podname}"
+    @success_file = "#{backup_dest_root}/#{@project}/#{@dc}/success"
 
     @backup_type = @metadata['labels']['backup_type']
     @backup_src = @metadata['annotations']['backup_src'].split(':') if @metadata['annotations'].key? 'backup_src'
@@ -58,92 +70,134 @@ class PodBackup
     @pod_spec['spec']['containers'].each {|c| @containers[c['name']] ||= {} }
   end
 
-  def container_backup_dir (container_name, root=@backup_dest_root)
-    if @backup_type == 'etcd' then
-      backup_dir = "#{root}/#{@project}/#{@podname}/#{container_name}/#{Time.now.strftime("%Y-%m-%d_%H:%M")}"
+  def log(msg)
+    log.info msg # will log to both STDOUT and file
+  end
+
+  def system_wrapper(cmd)
+    @log.debug "Running command: #{cmd}"
+    if FAKESYSTEM then
+      @log.info "Mocking success from: #{cmd}"
+      return true
     else
-      backup_dir = "#{root}/#{@project}/#{@podname}/#{container_name}/#{Time.now.strftime("%Y-%m")}"
+      return system(cmd)
     end
-    return backup_dir
+  end
+
+  def container_backup_dir (container_name, root=@backup_dest_pod_root)
+    "#{root}/#{container_name}"
+  end
+
+  # this assumes that "root" maps to <project>/<dc> on gluster
+  def etcd_local_backup_dir (container_name, root=@local_backup_dest)
+    "#{root}/#{@backup_time}/#{@podname}/#{container_name}"
   end
 
   def create_backup_dir (container_name)
-    puts "mkdir_p on #{container_backup_dir(container_name)}" if DEBUG
+    @log.debug "mkdir_p and chown_R on #{container_backup_dir(container_name)}"
     FileUtils.mkdir_p container_backup_dir(container_name), :mode => 0700
     FileUtils.chown_R @runasuser, @runasuser, container_backup_dir(container_name) if defined? @runasuser
   end
 
-  def commit_backup (dir)
-    system "cd #{dir} && git init" unless Dir.exists? "#{dir}/.git"
-    system "cd #{dir} && git add * && git commit -a -m 'Backup commited at #{Time.now}' > /dev/null 2>&1"
+  def project_dc_chown
+    FileUtils.chown_R @runasuser, @runasuser, "#{@backup_dest_root}/#{@project}/#{@dc}" if defined? @runasuser
   end
 
-  def age (container) #returns age of last git log entry in hours
-    gitdate = `cd #{container_backup_dir(container['name'])} && git log -1 --format="%ad" --date=local 2>/dev/null`
-    begin
-      lastlogentry = Time.parse(gitdate.to_i)
-    rescue
-      lastlogentry = 0
+  def touch_success_file
+    FileUtils.touch @success_file
+  end
+
+  def get_success_time
+    if File.exists?(@success_file)
+      File.stat(@success_file).mtime
+    else
+      Time.at(0)
     end
-    return ((Time.now.to_i - lastlogentry)/60.0/60).round 1
+  end
+
+  def age
+    last = Time.parse(get_success_time)
+    return ((Time.now.to_i - last)/60.0/60).round 1
+  end
+
+  def last_success
+    get_success_time
+  end
+
+  # Returns true if pod backups are within 24 hours, false otherwise
+  def within_24?
+    all_good = true
+    yesterday = Time.at(Time.now.to_i - 86400)
+    if get_success_time < yesterday then
+      all_good = false
+    end
+    all_good
+  end
+
+  def remove_old_backups
+    Dir["#{@backup_dest_pod_root}/*"].select {|x| x.split("/").last =~ /^(backup_)?[0-9_\-:]+(\.log)?$/ }.each { |d| check_age_and_remove d }
+    dir = "#{@backup_dest_root}/#{fqdn}/local"
+    # backup_2016-07-18_14:30
+    Dir["#{dir}/*"].select {|x| x.split("/").last =~ /^backup_[0-9_\-:]+(\.tar\.gz$|\.log$)/ }.each { |d| check_age_and_remove d }
+    return true
   end
 
   def backup
-    ret = -1
+    ret = false
+    if File.exists? @backup_dest_pod_root then
+      @log.info "Skipping #{@podname} as backup with same timestamp already exists"
+      return true
+    end
     @pod_spec['spec']['containers'].each do |container|
 
       case @backup_type
         when 'mysql'
-          puts "backing up mysql" if DEBUG
           create_backup_dir container['name']
           ret = backup_mysql container
         when 'rsync'
           create_backup_dir container['name']
           ret = backup_rsync container
         when 'etcd'
-          puts "something etcd" if DEBUG
           create_backup_dir container['name']
           ret = backup_etcd container
         else
-          puts "no backup_type for #{container['name']}"
+          @log.info "no backup_type for #{container['name']}"
           ret = false
       end
 
-      gitcommitret = commit_backup container_backup_dir(container['name'])
-      unless gitcommitret then
-        @containers[container['name']]['errors'] ||= []
-        @containers[container['name']]['errors'] << 'git commit failed'
-      end
-
-      @containers[container['name']]['age'] = age(container)
       @containers[container['name']]['success'] = ret
     end
 
-    return @containers
+    overall_success = true
+    @containers.each do |container,v|
+      overall_success = false unless v['success']
+    end
+
+    touch_success_file if overall_success
+
+    return overall_success
   end
 
   def backup_etcd (container)
-
-    local_backup_subdir = Time.now.strftime("%Y-%m-%d_%H:%M")
+  # local backup dir needs to map to gluster share /storage/infra01/infra-backups/<project>/<dc>
 
     backup_path = container_backup_dir container['name']
     logfile = "#{backup_path}/#{container['name']}.log"
     cmdfile = "#{backup_path}/#{container['name']}.cmd"
-    backup_cmd = "oc exec -n #{@project} #{@podname} > #{logfile} 2>&1 -- "
+    backup_cmd = "oc exec -n #{@project} #{@podname} -c #{container['name']} > \"#{logfile}\" 2>&1 -- "
     backup_cmd << "/etcdctl backup"
-    backup_cmd << " --data-dir=#{@backup_src[0]}"
-    backup_cmd << " --backup-dir=#{@local_backup_dest}/#{local_backup_subdir}"
+    backup_cmd << " --data-dir=\"#{@backup_src[0]}\""
+    backup_cmd << " --backup-dir=\"#{etcd_local_backup_dir container['name']}\""
 
-    puts "Running: #{backup_cmd}" if DEBUG
+    @log.debug "Running: #{backup_cmd}"
 
     File.open(cmdfile, 'w') { |f| File.write f, "#{backup_cmd}\n" }
 
-    ret = system(backup_cmd)
-    return ret
+    project_dc_chown
+    return system_wrapper(backup_cmd)
   end
 
   def backup_mysql (container)
-
     backup_path = "#{container_backup_dir container['name']}"
 
     metapw = container['env'].select { |env| env['name'] == 'MYSQL_ROOT_PASSWORD' }
@@ -153,17 +207,17 @@ class PodBackup
     mysqldump_cmd = "mysqldump -u root --all-databases"
     mysqldump_cmd += " -p\\\$#{root_pw}" unless root_pw.empty?
 
-    sqlfile = "#{backup_path}/#{container['name']}.sql"
+    sqlfile = "#{backup_path}/#{container['name']}.sql.gz"
     logfile = "#{backup_path}/#{container['name']}.log"
     cmdfile = "#{backup_path}/#{container['name']}.cmd"
 
-    backup_cmd = "oc exec -n #{@project} #{@podname} -c #{container['name']} -- bash -c \"#{mysqldump_cmd}\" 2> #{logfile} > #{sqlfile}"
-    puts "Running: #{backup_cmd}" if DEBUG
+    backup_cmd = "oc exec -n #{@project} #{@podname} -c #{container['name']} 2> \"#{logfile}\" -- "
+    backup_cmd << "bash -c \"#{mysqldump_cmd}\" | gzip > \"#{sqlfile}\""
+    @log.debug "Running: #{backup_cmd}"
 
     File.open(cmdfile, 'w') { |f| File.write f, "#{backup_cmd}\n" }
 
-    ret = system(backup_cmd)
-    return ret
+    return system_wrapper(backup_cmd)
   end
 
   def backup_rsync (container)
@@ -171,42 +225,69 @@ class PodBackup
     backup_path = "#{container_backup_dir container['name']}"
 
     if defined? @backup_src then
-      source_paths = @backup_src.join(' ')
+      source_paths += " " + @backup_src.map{ |e| '"' + e + '"' }.join(' ')
     else
-      source_paths = '/'
+      source_paths = '"/"'
     end
 
     tarfile = "#{backup_path}/#{container['name']}.tar.gz"
     logfile = "#{backup_path}/#{container['name']}.log"
     cmdfile = "#{backup_path}/#{container['name']}.cmd"
 
-    backup_cmd = "oc exec -n #{@project} #{@podname} -c #{container['name']} -- bash -c \"tar czf - #{source_paths}\" 2> #{logfile} 1> #{tarfile}"
-    puts "Running: #{backup_cmd}" if DEBUG
+    backup_cmd = "oc exec -n #{@project} #{@podname} -c #{container['name']} -- "
+    backup_cmd << "bash -c \"tar czf - #{source_paths}\" 2> \"#{logfile}\" 1> \"#{tarfile}\""
+    @log.debug "Running: #{backup_cmd}"
 
     File.open(cmdfile, 'w') { |f| File.write f, "#{backup_cmd}\n" }
 
-    ret = system(backup_cmd)
-    return ret
+    return system_wrapper(backup_cmd)
   end
 
 end
 
 ##########################################################################
+@yaml_loaded = false
 
-YAML.load(`oc get pods --all-namespaces  -l needs_backup="yes" -o yaml`)['items'].each do |pod|
-
-  p = PodBackup.new pod, BACKUP_DEST_ROOT
-  ret = p.backup
-  ret.each do |k,v|
-    msg = "Backup of #{pod['metadata']['namespace']} - #{pod['metadata']['name']} - #{k} "
-    msg += "completed successfully." if v['success']
-    msg += "FAILED." unless v['success']
-    puts msg
-  end
-  PP.pp ret
-
+def load_pod_yaml
+  YAML.load(`oc get pods --all-namespaces  -l needs_backup="yes" -o yaml`)
+  @yaml_loaded = true
 end
 
-exit 0
+if ARGV.include? 'status' then
+  load_pod_yaml
+  all_good = true
+  y['items'].each do |pod|
+    p = PodBackup.new pod, BACKUP_DEST_ROOT, log
+    all_good = false unless p.within_24?
+  end
+  puts all_good
+  exit all_good
+end
+
+if ARGV.include? 'purge' then
+  load_pod_yaml
+  y['items'].each do |pod|
+    p = PodBackup.new pod, BACKUP_DEST_ROOT, log
+    puts p.remove_old_backups
+  end
+end
+
+if ARGV.include? 'run' then
+  load_pod_yaml
+  all_good = true
+  y['items'].each do |pod|
+    log.info "Backing up pod #{pod['metadata']['name']}"
+    p = PodBackup.new pod, BACKUP_DEST_ROOT, log
+    ret = p.backup
+    log.info "pod:success: #{p.last_success}"
+    all_good = false unless ret
+    unless ret then
+      log.info "Backup for pod: #{pod['metadata']['name']} failed"
+    end
+
+  end
+  puts all_good
+  exit all_good
+end
 
 ##########################################################################
