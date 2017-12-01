ec1659bd08b038ec95367a27b8b3ca3b0f606ff7
greeninja <nick@greeninja.co.uk>
2017-12-01 13:06:46 +0000

    Update backup_script.rb

--- files/backup_script.rb
+++ files/backup_script.rb
@@ -20,6 +20,12 @@ if ENV.include? 'DEBUG' then
 else
   DEBUG=false
 end
+  
+if ENV.include? 'MASTERURL' then
+  MASTERURL=ENV['MASTERURL']
+else
+  MASTERURL="openshift-cluster.fhpaas.fasthosts.co.uk"
+end
 
 @global_logger = Logger.new("| tee backup_script.log")
 if DEBUG then
@@ -270,7 +276,7 @@ end
 def oc_login(silent=false)
   if File.exists? '/var/run/secrets/kubernetes.io/serviceaccount/token' then
     @global_logger.info "Logging into openshift with serviceaccount" unless silent
-    ret = system('oc login https://openshift-cluster.fhpaas.fasthosts.co.uk:8443 --token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token) > /dev/null 2>&1')
+    ret = system("oc login https://#{MASTERURL}:8443 --token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token) > /dev/null 2>&1")
   end
   ret = system('oc status >/dev/null 2>&1')
   die "Unable to log into openshift" unless ret
